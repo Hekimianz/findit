9858d5be9660f47cb637d09568f0bd17edb65e48
Aram Hekimian <guerrerohek121@gmail.com>
2025-02-20 07:57:12 -0600

    Add get single level endpoint to api

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -1,11 +1,24 @@
 const prisma = require("../config/prismaConfig");
 
-exports.GetLevels = async (req, res) => {
+exports.getLevels = async (req, res) => {
   try {
     const levels = await prisma.level.findMany();
     res.status(200).json({ levels });
   } catch (err) {
     console.error("Error fetching levels");
+    res.status(500).json({ error: err });
+  }
+};
+
+exports.getLevel = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const level = await prisma.level.findUnique({
+      where: { id },
+    });
+    res.status(200).json({ level });
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ error: err });
   }
 };
--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 const controller = require("../controllers/api");
 
-route.get("/", controller.GetLevels);
+route.get("/", controller.getLevels);
+route.get("/:id", controller.getLevel);
 
 module.exports = route;
